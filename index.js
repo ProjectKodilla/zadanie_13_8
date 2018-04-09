08e0a4a800f234de6f93dd6c6dbcda036179b88d
Marcin Bogdanski <marcin@bogdanski.info>
2018-04-09 22:52:38 +0200

    correct and finish

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer();
-server.on('request', function(request, response) {
-	response.setHeader("Content-Type", "text/html; charset=utf-8");
-	if (request.method === 'GET' && request.url === '/') {
-	 	fs.readFile('./index.html', 'utf-8', function(err, data) {
-	 		response.statusCode = 200;
-			response.write(data);
-			response.end();
-	 	});
-	} else {
-	 	response.statusCode = 404;
-	 	response.write("<img src='../404.jpg'/>");
-	 	response.end();
-	};
-});	
 
+server.on('request', function(request, response) {
+  if (request.method === 'GET' && request.url === '/') {
+    fs.readFile('./index.html', 'utf-8', function(err, data) {
+      response.writeHead(200, {
+        "Content-Type": "text/html; charset=utf-8"
+      });
+      if (!err) response.write(data);
+      response.end();
+    });
+
+  } else {
+    fs.readFile('./404.jpg', function(err, data) {
+      response.statusCode = 404;
+      if (!err) response.write(data);
+      response.end();
+    });
+  }
+});
 server.listen(8080);
